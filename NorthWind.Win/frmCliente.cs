684b350ed36cac86e654e3b8c7cf43c9276ba1e3
Jack Hugo <jack_hugo@hotmail.com>
2015-04-15 20:56:24 -0500

    Boton mostrar Clientes

--- NorthWind.Win/frmCliente.cs
+++ NorthWind.Win/frmCliente.cs
@@ -14,6 +14,8 @@ namespace NorthWind.Win
 {
     public partial class frmCliente : Form
     {
+        public event EventHandler<TbClienteBE> OnClienteSeleccionado;
+
         public frmCliente()
         {
             InitializeComponent();
@@ -27,6 +29,33 @@ namespace NorthWind.Win
             TbClientebindingSource.DataSource = Lista;
             dataGridView1.SelectionMode = 
                 DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private void AgregarClienteFactura()
+        { 
+        //agregar cliente al formulario documento
+            int i = dataGridView1.CurrentRow.Index;
+            int codigocliente= Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value);
+            //query en LINQ
+            TbClienteBE oCliente = (from item in Lista.ToArray()
+                                    where item.CodCliente == codigocliente
+                                    select item).Single();
+            OnClienteSeleccionado(new object(),oCliente);
+            this.Close();
+        }
+
+        private void button1_Click(object sender, KeyEventArgs e)
+        {
+            //Boton agrgar
+            AgregarClienteFactura();
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData ==Keys.Return)
+            {
+                AgregarClienteFactura();
+            }
         }
     }
 }
